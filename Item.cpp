17fc95e364cfc0ba9c5732e5d485be0a12886dee
Hytm4n <69047431+Hytm4n@users.noreply.github.com>
2024-04-18 08:05:44 +0700

    Item.cpp

--- Item.cpp
+++ Item.cpp
@@ -2,24 +2,85 @@
 #include<iostream>
 #include<string>
 
-	Item::Item(){
-		
+	Item::Item(int newID){
+		this->name = "";
+		this->type = "";
+		this->level = -1;
+		this->money = -1;
+		this->health = 0;
+		this->attack = 0;
+		this->defend = 0;
+		this->ID = newID;
 	}
-	std::string getName() const;
-	std::string getType() const;
-	int getLevel() const;
-	int getMoney() const;
-	int getHealth() const;
-	int getAttack() const;
-	int getDefend() const;
+	Item::Item(std::string newName, std::string newType, int newLevel, int newMoney, int newHealth, int newAttack, int newDefend, int newID){
+		this->name = newName;
+		this->type = newType;
+		this->level = newLevel;
+		this->money = newMoney;
+		this->health = newHealth;
+		this->attack = newAttack;
+		this->defend = newDefend;
+		this->ID = newID;
+	}
+	std::string Item::getName() const {
+		return name;
+	}
+	std::string Item::getType() const {
+		return type;
+	}
+	int Item::getLevel() const {
+		return level;
+	}
+	int Item::getMoney() const {
+		return money;
+	}
+	int Item::getHealth() const {
+		return health;
+	}
+	int Item::getAttack() const {
+		return attack;
+	}
+	int Item::getDefend() const {
+		return defend;
+	}
+	int Item::getID() const {
+		return ID;
+	}
 	//Getter
-	void setName();
-	void setType();
-	void setLevel();
-	void setMoney();
-	void setHealth();
-	void setAttack();
-	void setDefend();
+		this->name = newName;
+		this->type = newType;
+		this->level = newLevel;
+		this->money = newMoney;
+		this->health = newHealth;
+		this->attack = newAttack;
+		this->defend = newDefend;
+		this->ID = newID;
+	void Item::setName(std::string newName) {
+		this->name = newName;
+	}
+	void Item::setType(std::string newType) {
+		this->type = newType;
+	}
+	void Item::setLevel(int newLevel) {
+		this->level = newLevel;
+	}
+	void Item::setMoney(int newMoney) {
+		this->money = newMoney;
+	}
+	void Item::setHealth(int newHealth) {
+		this->health = newHealth;
+	}
+	void Item::setAttack(int newAttack) {
+		this->attack = newAttack;
+	}
+	void Item::setDefend(int newDefend) {
+		this->defend = newDefend;
+	}
+	void Item::setID(int newID) {
+		this->ID = newID;
+	}
 
 	//Other methods
-	void deleteItem();
+	void Item::deleteItem() {
+		delete this;
+	}
