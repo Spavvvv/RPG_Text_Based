186338e11de9d135abefc1287f632f5f70b08e4a
Hytm4n <69047431+Hytm4n@users.noreply.github.com>
2024-04-18 07:42:23 +0700

    Item.cpp

--- Item.cpp
+++ Item.cpp
@@ -1,1 +1,22 @@
 #include "Item.h"
+#include<iostream>
+#include<string>
+
+	std::string getName() const;
+	std::string getType() const;
+	int getLevel() const;
+	int getMoney() const;
+	int getHealth() const;
+	int getAttack() const;
+	int getDefend() const;
+	//Getter
+	void setName();
+	void setType();
+	void setLevel();
+	void setMoney();
+	void setHealth();
+	void setAttack();
+	void setDefend();
+
+	//Other methods
+	void deleteItem();
